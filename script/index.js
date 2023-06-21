68952618157248117a5e25eea609e4a8b9c062aa
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2023-06-21 20:21:08 -0300

    navegação

--- script/index.js
+++ script/index.js
@@ -6,3 +6,16 @@ const members = [
   { name: 'Rocket Raccoon', id: 'rocket' },
   { name: 'I am Groot', id: 'groot' },
 ]
+
+let activeMember = 0
+const images = document.getElementById('images')
+
+function navigationMember(direction) {
+  activeMember = activeMember + direction
+
+  const member = members[activeMember]
+
+  console.log(activeMember, member);
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)` 
+}
